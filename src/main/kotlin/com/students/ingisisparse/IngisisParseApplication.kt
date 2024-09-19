8e129af41620bcb7e13a32b0bb81aeabef7b208c
TomasValleDuran <tvalleduran@mail.austral.edu.ar>
2024-09-19 17:03:24 -0300

    Initial commit

---
+++ src/main/kotlin/com/students/ingisisparse/IngisisParseApplication.kt
@@ -0,0 +1,11 @@
+package com.students.ingisisparse
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class IngisisParseApplication
+
+fun main(args: Array<String>) {
+    runApplication<IngisisParseApplication>(*args)
+}
