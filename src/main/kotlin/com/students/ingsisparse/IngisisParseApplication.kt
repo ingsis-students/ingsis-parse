e774d46d5112db76fdda8f0c3797421e474225d5
TomasValleDuran <tvalleduran@mail.austral.edu.ar>
2024-10-18 13:21:00 -0300

    Eliminated dotenv

--- src/main/kotlin/com/students/ingsisparse/IngisisParseApplication.kt
+++ src/main/kotlin/com/students/ingsisparse/IngisisParseApplication.kt
@@ -1,6 +1,5 @@
 package com.students.ingsisparse
 
-import io.github.cdimascio.dotenv.Dotenv
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
@@ -8,7 +7,5 @@ import org.springframework.boot.runApplication
 class IngisisParseApplication
 
 fun main(args: Array<String>) {
-    val dotenv = Dotenv.load()
-    dotenv.entries().forEach { System.setProperty(it.key, it.value) }
     runApplication<IngisisParseApplication>(*args)
 }
