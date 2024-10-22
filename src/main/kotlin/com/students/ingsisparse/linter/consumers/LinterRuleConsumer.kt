e617d81792716cac98d4e6c60d56c2ec71bfd661
matias <mchialva@mail.austral.edu.ar>
2024-10-22 12:54:37 -0300

    REDIS: redis linter rule consumer

---
+++ src/main/kotlin/com/students/ingsisparse/linter/consumers/LinterRuleConsumer.kt
@@ -0,0 +1,32 @@
+package com.students.ingsisparse.linter.consumers
+
+import com.students.ingsisparse.linter.LintDto
+import org.austral.ingsis.redis.RedisStreamConsumer
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Profile
+import org.springframework.data.redis.core.ReactiveRedisTemplate
+import org.springframework.data.redis.stream.StreamReceiver
+import org.springframework.stereotype.Service
+import java.time.Duration
+import org.springframework.data.redis.connection.stream.ObjectRecord
+
+@Service
+@Profile("!test")
+class LintRuleConsumer @Autowired constructor(
+    redisTemplate: ReactiveRedisTemplate<String, String>,
+    @Value("\${stream.lint.key}") streamKey: String,
+    @Value("\${groups.lint}") groupId: String
+) : RedisStreamConsumer<LintDto>(streamKey, groupId, redisTemplate) {
+
+    override fun options(): StreamReceiver.StreamReceiverOptions<String, ObjectRecord<String, LintDto>> {
+        return StreamReceiver.StreamReceiverOptions.builder()
+            .pollTimeout(Duration.ofMillis(10000))
+            .targetType(LintDto::class.java)
+            .build()
+    }
+
+    override fun onMessage(record: ObjectRecord<String, LintDto>) {
+        println(record.value)
+    }
+}
