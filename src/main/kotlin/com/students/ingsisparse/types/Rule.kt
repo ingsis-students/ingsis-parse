305ad102fdb19bb6d9e27b275cf6d3a80e10eee4
matias <mchialva@mail.austral.edu.ar>
2024-11-07 10:39:44 -0300

    REDIS: rule default values for the deserialization process.

--- src/main/kotlin/com/students/ingsisparse/types/Rule.kt
+++ src/main/kotlin/com/students/ingsisparse/types/Rule.kt
@@ -1,8 +1,8 @@
 package com.students.ingsisparse.types
 
 data class Rule(
-    val id: String,
-    val name: String,
-    val isActive: Boolean,
+    val id: String = "",
+    val name: String = "",
+    val isActive: Boolean = false,
     val value: Any? = null
 )
