2f2003e2097df9c61e4495b9956b8a5c7889a027
matias <mchialva@mail.austral.edu.ar>
2024-11-11 13:12:50 -0300

    format endpoint.

--- src/main/kotlin/com/students/ingsisparse/formatter/FormatterController.kt
+++ src/main/kotlin/com/students/ingsisparse/formatter/FormatterController.kt
@@ -1,16 +1,20 @@
 package com.students.ingsisparse.formatter
 
-import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.PostMapping
 
 @RestController
 @RequestMapping("/api/printscript")
 class FormatterController(private val formatterService: FormatterService) {
 
     @PostMapping("/format")
-    fun formatCode(@RequestBody formatDto: FormatDto): String {
+    fun formatCode(
+        @RequestHeader("Authorization") token: String,
+        @RequestBody formatDto: FormatDto
+    ): String {
         val version = formatDto.version
         val code = formatDto.code
         val rules = formatDto.rules.toString()
